b0333c0348ade4211dbfcd4b5887245b2ca4a99e
Chaitanya Bandiwdekar <c.bandiwdekar@somaiya.edu>
2021-07-24 23:03:22 +0530

    redux boilerplate done

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavBar';
 import ToDoList from './components/ToDoList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ToDoList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ToDoList />
+      </div>
+    </Provider>
   );
 }
 
